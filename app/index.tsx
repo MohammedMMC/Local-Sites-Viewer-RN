6c9c95ee374a937d5822bec431f7d6248ae43a44
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-12-21 00:14:33 +0300

    v1.0 - Done

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { Colors } from "@/constants/Colors";
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View, FlatList } from "react-native";
+import * as Network from 'expo-network';
 
 export default function Index() {
   const [hosts, setHosts] = useState([
@@ -8,18 +9,75 @@ export default function Index() {
     new HostData("192.168.1.1"),
     new HostData("192.168.1.1")
   ]);
+  const [loadingPercent, setLoadingPercent] = useState(0);
+  const [isScanning, setIsScanning] = useState(false);
+  const [ports, setPorts] = useState([80, 5500, 3300, 5050, 3030, 8080, 8081, 8082]);
+
+  const fetchWithTimeout = (url: string, timeout: number = 2000) => Promise.race([
+    fetch(url, { method: "GET" }),
+    new Promise((_, reject) =>
+      setTimeout(() => reject(new Error('Timeout')), timeout)
+    )
+  ]);
+
+  const getConnectedDevices = async () => {
+    setIsScanning(true);
+    setHosts([]);
+
+    try {
+      const networkState = await Network.getNetworkStateAsync();
+      if (!networkState.isConnected) {
+        console.log('Not connected to a network');
+        return;
+      }
+      const baseIP = (await Network.getIpAddressAsync()).split('.').slice(0, 3).join('.');
+      const ips2Check = Array.from({ length: 255 }, (_, i) => `${baseIP}.${i}`);
+      const scanned: HostData[] = [];
+
+      for (let i = 0; i < ips2Check.length; i += 255 / (ports.length + 1)) {
+        setLoadingPercent(Math.round((i / 250) * 100));
+        const ipsBatch = ips2Check.slice(i, i + 255 / (ports.length + 1));
+        scanned.push(...(await Promise.all(
+          ipsBatch.map(addr =>
+            Promise.all(ports.map(port =>
+              fetchWithTimeout(`http://${addr}:${port}`)
+                .then(async (res: Response | any) =>
+                  new HostData(`${addr}:${port}`,
+                    (await res.text()).match(/<title>(.*?)<\/title>/i)?.[1] ?? null
+                  )
+                ).catch(() => null)
+            ))
+          )
+        )).flat().filter(ip => ip instanceof HostData));
+
+        console.log(ipsBatch, scanned);
+
+        await new Promise(resolve => setTimeout(resolve, 100));
+      }
+
+      console.log(scanned);
+      setHosts(scanned);
+    } catch (error) {
+      console.error("Error scanning network:", error);
+    } finally {
+      setLoadingPercent(0);
+      setIsScanning(false);
+    }
+  }
 
   return (
     <View style={styles.body}>
       <View style={{ paddingHorizontal: 18 }}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Load Local Sites</Text>
+        <TouchableOpacity style={styles.button}
+          onPress={!isScanning ? getConnectedDevices : () => { }}
+          disabled={isScanning}>
+          <Text style={styles.buttonText}>{isScanning ? `Loading... - ${loadingPercent}%` : "Load Local Sites"}</Text>
         </TouchableOpacity>
       </View>
 
       <FlatList style={cardsStyle.list} data={hosts} renderItem={({ item }) =>
         <View style={cardsStyle.card}>
-          <Text style={cardsStyle.title}>{item.title ?? "No Title"}</Text>
+          <Text style={cardsStyle.title}>{item.title || "No Title"}</Text>
           <Text style={cardsStyle.text}>{item.ip}</Text>
           <View style={{
             flex: 1,
