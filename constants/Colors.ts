ebe02933e9ccdd22004229b2c12148e06c3fc7ec
Mohamed Mahmodi <103428364+MohammedMMC@users.noreply.github.com>
2024-12-23 21:16:15 +0300

    changed some colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -2,8 +2,8 @@
 
 export const Colors = {
     red: "#990c39",
-    primaryDarker: "#0a6d8e",
-    primary: "#0a7ea4",
+    primaryDarker: "#3493b3",
+    primary: "#3a9fc1",
     text: '#11181C',
     background: '#f4fafa',
 };
